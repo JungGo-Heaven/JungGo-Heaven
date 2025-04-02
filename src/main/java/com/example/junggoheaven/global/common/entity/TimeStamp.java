015d17d61f85a463db85c6db623f0f0fdbab567d
khw00185 <ksng0185@naver.com>
2025-04-02 14:45:03 +0900

    initail commit

--- src/main/java/com/example/junggoheaven/global/common/entity/TimeStamp.java
+++ src/main/java/com/example/junggoheaven/global/common/entity/TimeStamp.java
@@ -1,4 +1,4 @@
-package com.example.junggoheaven.common.entity;
+package com.example.junggoheaven.global.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
