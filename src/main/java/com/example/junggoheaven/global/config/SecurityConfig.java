c395a53b88df89e85f5aae624940ff63f958dfdf
euuns <dmsu2@naver.com>
2025-04-02 22:07:49 +0900

    Feat: Spring Security & AOP & OAuth2.0 연결을 위한 설정 추가
    1. Handler는 oauth 성공/실패 시 동작
    2. Handler가 동작하면 log를 찍기 위한 AOP 작성 (@Handler 어노테이션 추가)

--- src/main/java/com/example/junggoheaven/global/config/SecurityConfig.java
+++ src/main/java/com/example/junggoheaven/global/config/SecurityConfig.java
@@ -3,8 +3,25 @@ package com.example.junggoheaven.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.example.junggoheaven.global.auth.handler.OAuth2AuthenticationFailureHandler;
+import com.example.junggoheaven.global.auth.handler.OAuth2AuthenticationSuccessHandler;
+import com.example.junggoheaven.global.filter.ExceptionHandlerFilter;
+import com.example.junggoheaven.global.filter.JwtAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,9 +31,53 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final ExceptionHandlerFilter exceptionHandlerFilter;
+	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+	private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+		return new HttpSessionOAuth2AuthorizationRequestRepository();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http,
+		OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2Service) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+
+			.sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+			.addFilterBefore(jwtAuthenticationFilter, SecurityContextHolderAwareRequestFilter.class)
+			.addFilterBefore(exceptionHandlerFilter, JwtAuthenticationFilter.class)
+
+			.formLogin(AbstractHttpConfigurer::disable)
+			.anonymous(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.rememberMe(AbstractHttpConfigurer::disable)
+
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(new AntPathRequestMatcher("/api/*/auth/**")).permitAll()
+				.anyRequest().authenticated())
+
+			.oauth2Login(oauth2 -> oauth2
+				.authorizationEndpoint(endpoint -> endpoint
+					.authorizationRequestRepository(authorizationRequestRepository())
+				)
+				.userInfoEndpoint(userInfo -> userInfo
+					.userService(oauth2Service))
+				.successHandler(oAuth2AuthenticationSuccessHandler)
+				.failureHandler(oAuth2AuthenticationFailureHandler)
+			)
+
+			.build();
+	}
 }
