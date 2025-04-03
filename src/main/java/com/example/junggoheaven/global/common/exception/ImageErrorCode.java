f149bd7ed98251a6c30b8faaf866def6b5e6806f
Seung(emily_the_dev) <emily101304@naver.com(emily101304)>
2025-04-03 15:29:49 +0900

    Feat : Image 도메인에 사용되는 에러코드 생성 Mod : BaseExeption 필드에 final 추가 및 생성자에 public 추가

---
+++ src/main/java/com/example/junggoheaven/global/common/exception/ImageErrorCode.java
@@ -0,0 +1,33 @@
+package com.example.junggoheaven.global.common.exception;
+
+import static org.springframework.http.HttpStatus.*;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ImageErrorCode implements ErrorCode {
+
+    TYPE_MISMATCH("TYPE_MISMATCH", BAD_REQUEST, "파일 형식이 올바르지 않습니다.");
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getDefaultMessage() {
+        return this.message;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return this.httpStatus;
+    }
+}
