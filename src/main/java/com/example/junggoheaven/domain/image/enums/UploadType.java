2aa43955a892c2f5ea13dfd94c3fb50586d0adac
Seung(emily_the_dev) <emily101304@naver.com(emily101304)>
2025-04-03 15:30:45 +0900

    Feat : 이미지 업로드 타입 이넘 타입으로 구현

---
+++ src/main/java/com/example/junggoheaven/domain/image/enums/UploadType.java
@@ -0,0 +1,46 @@
+package com.example.junggoheaven.domain.image.enums;
+
+import static com.example.junggoheaven.domain.user.enums.UserRole.*;
+
+import com.example.junggoheaven.domain.user.enums.UserRole;
+import com.example.junggoheaven.global.common.exception.BaseException;
+import com.example.junggoheaven.global.common.exception.ImageErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum UploadType {
+
+    PROFILES("profiles", role -> role == ROLE_USER || role == ROLE_GUEST),
+    PRODUCTS("products", role -> role == ROLE_USER || role == ROLE_GUEST),
+    CHAT_ROOMS("chat_rooms", role -> role == ROLE_USER || role == ROLE_GUEST);
+
+    private final String type;
+
+    // if 분기처리 제거할 수 있고, 폴더 타입에 따른 권한을 바로 나눌 수 있다.
+    private final Predicate<UserRole> roleChecker;
+
+    // 정의되어 있는 타입인지 체크
+    public static UploadType from(String type) {
+        for (UploadType uploadType : values()) {
+            if (uploadType.type.equalsIgnoreCase(type)) {
+                return uploadType;
+            }
+        }
+        throw new BaseException(ImageErrorCode.TYPE_MISMATCH);
+    }
+
+    // url의 일부로 생성
+    public String getPrefix() {
+        return this.type + "/";
+    }
+
+    // Predicate 사용 메서드
+    public boolean isAllowedFor(UserRole role) {
+        return roleChecker.test(role);
+    }
+
+}
