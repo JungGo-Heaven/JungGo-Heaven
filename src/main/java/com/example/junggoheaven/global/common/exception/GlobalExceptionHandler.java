a619001ee44400b433cb40de22b0495833192315
euuns <dmsu2@naver.com>
2025-04-07 18:10:59 +0900

    Fix/Feat: 예외 발생 시 강제 redirect 수정
    - sendError 전달 시 강제 redirect
    - sendError를 전달하지 않고 예외를 발생하도록 임시 구현
    - BaseException를 잡으면 error 상황을 json으로 반환하고, AccessDeniedException 발생 시 FORBIDDEN 반환
    추후 front에게 에러 상황을 전달하기 위한 로직 구현이 필요함
    
    #7

--- src/main/java/com/example/junggoheaven/global/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/junggoheaven/global/common/exception/GlobalExceptionHandler.java
@@ -5,18 +5,23 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.junggoheaven.global.common.response.ErrorResponseDto;
 import com.example.junggoheaven.global.common.response.ValidResponseDto;
 
-import lombok.extern.slf4j.Slf4j;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
-@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+	private final HttpServletResponse httpServletResponse;
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<List<ValidResponseDto>> invalidRequestExceptionException(
@@ -31,5 +36,16 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 	}
 
+	@ExceptionHandler(BaseException.class)
+	public ResponseEntity<ErrorResponseDto> handleBaseException(BaseException ex) {
+		ErrorResponseDto error = ErrorResponseDto.of(ex);
+		return new ResponseEntity<>(error, ex.getStatus());
+	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<ErrorResponseDto> handleAccessDeniedException(AccessDeniedException ex) {
+		ErrorResponseDto error = ErrorResponseDto.of(ex.getClass().getSimpleName(), "Invalid permission. 해당 권한이 없습니다.");
+		return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+	}
 
 }
