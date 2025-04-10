3fcc1cff54f0f3da39d8313b0fb6826bd916c3ed
khw00185 <ksng0185@naver.com>
2025-04-10 18:23:59 +0900

    fix(websocket): StompJwtChannelIntercepter클래스 수정

--- src/main/java/com/example/junggoheaven/global/websocket/StompJwtChannelInterceptor.java
+++ src/main/java/com/example/junggoheaven/global/websocket/StompJwtChannelInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.util.StringUtils;
 
 import java.security.Principal;
@@ -18,7 +19,8 @@ public class StompJwtChannelInterceptor implements ChannelInterceptor {
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor accessor = MessageHeaderAccessor
+                .getAccessor(message, StompHeaderAccessor.class);
 
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
             String tokenValue = accessor.getFirstNativeHeader("Authorization");
