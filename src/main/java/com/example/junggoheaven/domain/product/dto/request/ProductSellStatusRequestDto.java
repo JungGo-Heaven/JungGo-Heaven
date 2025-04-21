984f22418faf7376592eec244fb0f620add01173
yjn33 <yjn0032@gmail.com>
2025-04-21 11:26:50 +0900

    feat: Valid 기능 추가
    RequestDto에 @Valid 추가

--- src/main/java/com/example/junggoheaven/domain/product/dto/request/ProductSellStatusRequestDto.java
+++ src/main/java/com/example/junggoheaven/domain/product/dto/request/ProductSellStatusRequestDto.java
@@ -2,6 +2,7 @@ package com.example.junggoheaven.domain.product.dto.request;
 
 
 import com.example.junggoheaven.domain.product.enums.SellStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ProductSellStatusRequestDto {
 
+	@NotNull(message = "판매상태를 선택해 주세요")
 	private final SellStatus sellStatus;
 
 }
