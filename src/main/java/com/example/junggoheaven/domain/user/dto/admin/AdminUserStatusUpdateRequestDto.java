338672b8bc39d611aca1a73d13fb885809600f3c
euuns <dmsu2@naver.com>
2025-04-22 21:42:09 +0900

    Feat: 결제 추가를 위해 User에 customerKey 추가

--- src/main/java/com/example/junggoheaven/domain/user/dto/admin/AdminUserStatusUpdateRequestDto.java
+++ src/main/java/com/example/junggoheaven/domain/user/dto/admin/AdminUserStatusUpdateRequestDto.java
@@ -4,11 +4,12 @@ import com.example.junggoheaven.domain.user.enums.UserStatus;
 import com.example.junggoheaven.global.common.annotation.ValidEnum;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class AdminUserStatusUpdateRequestDto {
-	@NotBlank(message = "변경하려는 사용자 id를 입력하세요.")
+	@NotNull(message = "변경하려는 사용자 id를 입력하세요.")
 	private Long userId;
 	@NotBlank(message = "변경하려는 상태 코드를 입력하세요.")
 	@ValidEnum(enumClass = UserStatus.class, message = "ACTIVE, WARNING, STOPPED, DELETED 중 하나를 입력해주세요.")
