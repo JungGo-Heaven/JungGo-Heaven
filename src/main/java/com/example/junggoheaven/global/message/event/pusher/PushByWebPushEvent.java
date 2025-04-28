164c8220998e0cb087388635117e57d9c0f03241
jhwang2 <ahkiler12@gmail.com>
2025-04-28 19:20:32 +0900

    fix: UserChannelMapper 수정

--- src/main/java/com/example/junggoheaven/global/message/event/pusher/PushByWebPushEvent.java
+++ src/main/java/com/example/junggoheaven/global/message/event/pusher/PushByWebPushEvent.java
@@ -11,11 +11,9 @@ import lombok.Getter;
 
 @Getter
 public class PushByWebPushEvent extends NotificationEvent {
-	private final List<MatchedUserDto> userList;
 
 	public PushByWebPushEvent(Object source, Long userId,
 		NotificationType notificationType, String notificationMessage) {
 		super(source, userId, notificationType, notificationMessage);
-		this.userList = new ArrayList<>();
 	}
 }
