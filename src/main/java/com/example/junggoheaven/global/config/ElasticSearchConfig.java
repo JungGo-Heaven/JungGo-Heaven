8287b02fc8230e40e79f568d853b1b4b359161d5
jhwang2 <ahkiler12@gmail.com>
2025-05-06 12:59:16 +0900

    fix: ElasticSearchConfig 수정

--- src/main/java/com/example/junggoheaven/global/config/ElasticSearchConfig.java
+++ src/main/java/com/example/junggoheaven/global/config/ElasticSearchConfig.java
@@ -2,6 +2,7 @@ package com.example.junggoheaven.global.config;
 
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
@@ -9,15 +10,19 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.example.junggoheaven.domain.user.repository")
+@RequiredArgsConstructor
 public class ElasticSearchConfig {
+	@Value("${spring.elasticsearch.uris}")
+	private String uris;
 
 	@Bean
 	public ElasticsearchClient elasticsearchClient() {
 		RestClient restClient = RestClient.builder(
-			new HttpHost("localhost", 9200, "http")
+			new HttpHost(uris, 9200, "http")
 		).build();
 
 		RestClientTransport transport = new RestClientTransport(
